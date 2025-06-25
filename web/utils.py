1f1e74b77286e739cef2bbb39948d3c499e88018
Luis Lamus <lamusluis23@gmail.com>
2025-06-25 14:03:51 -0400

    Cambios Simples

--- web/utils.py
+++ web/utils.py
@@ -24,7 +24,7 @@ index_meta = [
 ## APP CONSULTAS URL MALICIOSAS
 
 url_check_tittle = "Verificador de URLs Maliciosas"
-url_check_description = "Verifica URLs sospechosas y descubre si son maliciosas o seguras. Utiliza nuestro servicio para protegerte de amenazas en línea.",
+url_check_description = "Verifica URLs sospechosas y descubre si son maliciosas o seguras. Utiliza nuestro servicio para protegerte de amenazas en línea."
 
 url_check_meta = [
     {"name": "og:title", "content": url_check_tittle},
